27c585f78353f18025fe25d1c72c9863d2c9e80e
Haaben.exe <htkidanu@gmail.com>
2025-02-05 16:28:55 -0500

    updated wednesday

--- js/data.js
+++ js/data.js
@@ -1,1 +1,36 @@
 //=====================Store Objects Here=========================\\
+
+
+
+
+/*------------------------ Constants ------------------------------*/
+// let playerChoice
+let score = 0;
+let answerAccuracy = 0;
+
+
+/*--------------------- Variables (state) -------------------------*/
+
+
+
+/*------------------ Cached Element References --------------------*/
+
+
+
+/*------------------------- Functions -----------------------------*/
+
+
+
+/*----------------------- Event Listeners -------------------------*/
+const nextQuestion = document.querySelectorAll("button#next-button")
+
+
+const answerButtons = document.querySelectorAll("button#answer-buttons")
+
+const buttonStart = document.querySelector("button#Start")
+buttonStart.addEventListener("click", () => {
+    console.log("Button click confirmed");
+})
+
+const submitResult = document.querySelector("button#submit-results")
+
