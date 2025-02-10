fe474045d739c61100b7d1b08cd79ab934554050
Haaben.exe <htkidanu@gmail.com>
2025-02-10 09:37:00 -0500

    music still needed to be louder

--- js/app.js
+++ js/app.js
@@ -281,7 +281,7 @@ buttonStart.addEventListener("click", () => {
 
 })
 buttonStart.addEventListener("click", (event) => {
-    themeMusic.volume = .1
+    themeMusic.volume = .5
     themeMusic.play()
     themeMusic.addEventListener("ended", () => {
         themeMusic.currentTime = 0;
