8035d3472342aad720933220348dad7e6edf5b8f
Haaben.exe <htkidanu@gmail.com>
2025-02-10 09:43:19 -0500

    last time adjusting volume

--- js/app.js
+++ js/app.js
@@ -281,7 +281,7 @@ buttonStart.addEventListener("click", () => {
 
 })
 buttonStart.addEventListener("click", (event) => {
-    themeMusic.volume = .3
+    themeMusic.volume = .2
     themeMusic.play()
     themeMusic.addEventListener("ended", () => {
         themeMusic.currentTime = 0;
